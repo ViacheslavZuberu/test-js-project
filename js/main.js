365db85e68a16ba03b199983e34005706948febe
ViacheslavZuberu <clavik15@gmail.com>
2019-01-23 19:16:43 +0200

    Datepicker overview

--- js/main.js
+++ js/main.js
@@ -11,11 +11,27 @@ let options = {
     }
 };
 
-$("#birthday").datepicker({
+let dateOptions = {
     changeYear: true,
-    monthNames: ["Январь", "Февраль", "Март", "Апрель", "Май"],
+    monthNames: [
+        "Январь",
+        "Февраль",
+        "Март",
+        "Апрель",
+        "Май",
+        "Июнь",
+        "Июль",
+        "Август",
+        "Сентябрь",
+        "Октябрь",
+        "Ноябрь",
+        "Декабрь"
+    ],
+    firstDay: 1,
     dayNamesMin: ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"],
     dateFormat: "dd.mm.yy"
-});
+};
+
+$("#faryd").datepicker(dateOptions);
 $("#my-window").dialog(options);
 $("#tabs").tabs();
